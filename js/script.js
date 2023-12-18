506101eb52d1e39d28571792625c40c71c4403e2
caraja21122133131 <130501406+caraja21122133131@users.noreply.github.com>
2023-12-18 21:13:30 +0100

    µDAvid

---
+++ js/script.js
@@ -0,0 +1,1 @@
+var music=document.getElementById("music"),isPlaying=!0,button=document.getElementById("toggle");function togglePlay(){isPlaying?music.pause():music.play()}isPlaying&&button.classList.add("rainbow-text"),music.volume=.2,music.onplaying=function(){isPlaying=!0},music.onpause=function(){isPlaying=!1};
